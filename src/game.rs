8d03700a145f64affb2ce266c7d5444f8e0bde4e
David LeGare <dlegare.1001@gmail.com>
2019-08-22 18:46:07 -0500

    Setup game state

--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,7 @@
-use actix::prelude::*;
-use actix_web_actors::ws;
+use futures::channel::mpsc::UnboundedSender;
 use log::*;
 use std::{collections::HashMap, sync::atomic::*};
+use warp::ws;
 
 #[derive(Debug)]
 pub struct GameController {
@@ -17,29 +17,31 @@ impl GameController {
         }
     }
 
-    pub fn generate_player_id(&self) -> PlayerId {
+    pub fn next_player_id(&self) -> PlayerId {
         let id = self.id_counter.fetch_add(1, Ordering::SeqCst);
         PlayerId(id)
     }
-}
 
-impl Actor for GameController {
-    type Context = Context<Self>;
-}
-
-impl Handler<ClientConnected> for GameController {
-    type Result = MessageResult<ClientConnected>;
-
-    fn handle(&mut self, _message: ClientConnected, _ctx: &mut Self::Context) -> Self::Result {
+    pub fn handle_client_connected(&mut self, sender: UnboundedSender<ws::Message>) -> PlayerId {
         // Create a new player for the client and add it to the set of players.
-        let id = self.generate_player_id();
-        let player = Player {
-            id,
-            health: 10,
-        };
+        let id = self.next_player_id();
+        let player = Player { id, health: 10 };
         self.players.insert(id, player);
 
-        MessageResult(CreatePlayer(id))
+        id
+    }
+
+    pub fn handle_client_disconnected(&mut self, id: PlayerId) {
+        // TODO: Maybe do something about that?
+        info!("good bye user: {:?}", id);
+    }
+
+    pub fn player(&self, id: PlayerId) -> Option<&Player> {
+        self.players.get(&id)
+    }
+
+    pub fn player_mut(&mut self, id: PlayerId) -> Option<&mut Player> {
+        self.players.get_mut(&id)
     }
 }
 
@@ -53,10 +55,3 @@ pub struct Player {
     // The player's current health.
     pub health: u32,
 }
-
-#[derive(Debug, Message)]
-#[rtype(CreatePlayer)]
-pub struct ClientConnected;
-
-#[derive(Debug, Message)]
-pub struct CreatePlayer(pub PlayerId);
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,28 @@
-extern crate futures;
-extern crate pretty_env_logger;
-extern crate warp;
-
-use std::collections::HashMap;
-use std::sync::{
-    atomic::{AtomicUsize, Ordering},
-    Arc, Mutex,
+use crate::game::*;
+use futures::{select, pin_mut, channel::mpsc, compat::*, prelude::*};
+use log::*;
+use std::sync::{Arc, Mutex};
+use warp::{
+    ws::{Message, WebSocket},
+    Filter,
 };
 
-use futures::channel::mpsc;
-use futures::compat::*;
-use futures::prelude::*;
-use warp::ws::{Message, WebSocket};
-use warp::Filter;
+mod game;
 
-/// Our global unique user id counter.
-static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
-
-/// Our state of currently connected users.
-///
-/// - Key is their id
-/// - Value is a sender of `warp::ws::Message`
-type Users = Arc<Mutex<HashMap<usize, mpsc::UnboundedSender<Message>>>>;
+// TODO: Don't pass around references to the state, use channels to communicate with it.
+type State = Arc<Mutex<GameController>>;
 
 #[runtime::main(runtime_tokio::Tokio)]
 async fn main() {
-    pretty_env_logger::init();
+    std::env::set_var(
+        "RUST_LOG",
+        "click_battler=trace",
+    );
+    env_logger::init();
 
     // Keep track of all connected users, key is usize, value
     // is a websocket sender.
-    let users = Arc::new(Mutex::new(HashMap::new()));
+    let users = Arc::new(Mutex::new(GameController::new()));
     // Turn our "state" into a new Filter...
     let users = warp::any().map(move || users.clone());
 
@@ -40,7 +33,7 @@ async fn main() {
         .and(users)
         .map(|ws: warp::ws::Ws2, users| {
             // This will call our function if the handshake succeeds.
-            ws.on_upgrade(move |socket| Compat::new(Box::pin(user_connected(socket, users))))
+            ws.on_upgrade(move |socket| Compat::new(Box::pin(user_connected(socket, users).map(|_| Ok(())))))
         });
 
     // GET / -> index html
@@ -53,77 +46,36 @@ async fn main() {
         .expect("I guess an error happened in the server");
 }
 
-async fn user_connected(ws: WebSocket, users: Users) -> Result<(), ()> {
-    // Use a counter to assign a new unique ID for this user.
-    let my_id = NEXT_USER_ID.fetch_add(1, Ordering::Relaxed);
+async fn user_connected(ws: WebSocket, state: State) {
+    // Create a channel for receiving messages from the server.
+    let (server_sender, mut server_receiver) = mpsc::unbounded();
 
-    eprintln!("new chat user: {}", my_id);
+    // Split the socket into a sender and receiver of messages.
+    let (mut socket_sender, mut socket_receiver) = ws.sink_compat().split();
 
-    // Split the socket into a sender and receive of messages.
-    let (mut user_ws_tx, mut user_ws_rx) = ws.sink_compat().split();
+    // Allow the game state to handle the newly-connected client.
+    let player_id = state.lock().unwrap().handle_client_connected(server_sender);
+    info!("New client connected, assigned ID {:?}", player_id);
 
-    // Use an unbounded channel to handle buffering and flushing of messages
-    // to the websocket...
-    let (tx, mut rx) = mpsc::unbounded();
-    runtime::spawn(async move {
-        while let Some(item) = rx.next().await {
-            if let Err(err) = user_ws_tx.send(item).await {
-                eprintln!("websocket send error: {}", err);
+    let server_receiver = server_receiver.fuse();
+    let socket_receiver = socket_receiver.fuse();
+
+    pin_mut!(server_receiver, socket_receiver);
+
+    loop {
+        select! {
+            server_message = server_receiver.next() => {
+                info!("Received message from server: {:?}", server_message);
             }
-        }
-    });
 
-    // Save the sender in our list of connected users.
-    users.lock().unwrap().insert(my_id, tx);
+            socket_message = socket_receiver.next() => {
+                info!("Received message from socket: {:?}", socket_message);
+            }
 
-    // Return a `Future` that is basically a state machine managing
-    // this specific user's connection.
-
-    // Make an extra clone to give to our disconnection handler...
-    let users2 = users.clone();
-
-    while let Some(msg_result) = user_ws_rx.next().await {
-        match msg_result {
-            Ok(msg) => user_message(my_id, msg, &users),
-            Err(err) => eprintln!("websocket error(uid={}): {}", my_id, err),
+            complete => break,
         }
     }
-    user_disconnected(my_id, &users2);
 
-    Ok(())
-}
-
-fn user_message(my_id: usize, msg: Message, users: &Users) {
-    // Skip any non-Text messages...
-    let msg = if let Ok(s) = msg.to_str() {
-        s
-    } else {
-        return;
-    };
-
-    let new_msg = format!("<User#{}>: {}", my_id, msg);
-
-    // New message from this user, send it to everyone else (except same uid)...
-    //
-    // We use `retain` instead of a for loop so that we can reap any user that
-    // appears to have disconnected.
-    for (&uid, tx) in users.lock().unwrap().iter() {
-        if my_id != uid {
-            match tx.unbounded_send(Message::text(new_msg.clone())) {
-                Ok(()) => (),
-                Err(_disconnected) => {
-                    // The tx is disconnected, our `user_disconnected` code
-                    // should be happening in another task, nothing more to
-                    // do here.
-                }
-            }
-        }
-    }
-}
-
-fn user_disconnected(my_id: usize, users: &Users) {
-    eprintln!("good bye user: {}", my_id);
-
-    // Stream closed up, so remove from the user list
-    users.lock().unwrap().remove(&my_id);
+    // Stream closed up, so remove from the user list.
+    state.lock().unwrap().handle_client_disconnected(player_id);
 }
